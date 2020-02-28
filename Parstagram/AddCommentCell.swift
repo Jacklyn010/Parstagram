913f2c20009a568fc28ad2c4ad44231e0b40285c
Jacklyn <jih045@ucsd.edu>
2020-02-28 04:12:36 -0800

    ...

---
+++ Parstagram/AddCommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddCommentCell.swift
+//  Parstagram
+//
+//  Created by mac air on 2020/2/28.
+//  Copyright © 2020 mac air. All rights reserved.
+//
+
+import UIKit
+
+class AddCommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by mac air on 2020/2/28.
+//  Copyright © 2020 mac air. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
